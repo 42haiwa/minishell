651f0eb8da3c25a4cc83128ecc8c3a76ff4a3a21
Clement Jouenne <cjouenne@c2r6p5.42lehavre.fr>
2023-11-30 18:46:13 +0100

    started programming cd

--- src/cd.c
+++ src/cd.c
@@ -1,37 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cjouenne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/30 16:39:00 by cjouenne          #+#    #+#             */
-/*   Updated: 2023/11/30 18:45:46 by cjouenne         ###   ########.fr       */
+/*   Created: 2023/11/30 18:44:38 by cjouenne          #+#    #+#             */
+/*   Updated: 2023/11/30 18:45:27 by cjouenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "minishell.h"
 
-# define PROMPT "minishell $ "
-
-# include <stdio.h>
-# include <stdlib.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-
-# include "libft.h"
-
-typedef struct s_env
+void	cd(void)
 {
-	char *path;
-	char *pwd;
-}	t_env;
-
-//	builtins.c
-void	check_builtins(char const *buf);
-//	exit.c
-void	ft_exit(void);
-//	cd.c
-void	ft_cd(const char *path);
-#endif
+	
+}
