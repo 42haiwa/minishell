5ece20b8e94e28361122f527c92ecff72d813006
Clement Jouenne <cjouenne@c1r7p3.42lehavre.fr>
2023-12-01 16:22:24 +0100

    correct .h

--- include/minishell.h
+++ include/minishell.h
@@ -6,11 +6,7 @@
 /*   By: aallou-v <aallou-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 16:39:00 by cjouenne          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/12/01 15:26:25 by cjouenne         ###   ########.fr       */
-=======
-/*   Updated: 2023/12/01 14:47:33 by aallou-v         ###   ########.fr       */
->>>>>>> 4fd51051ba587b8b88fd6a129797d95f6e3638e1
+/*   Updated: 2023/12/01 16:22:13 by cjouenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,15 +48,11 @@ void	ft_cd(const char *path);
 void	parse_envp(t_core *core, char **envp);
 
 //	three
-<<<<<<< HEAD
 t_node	*node_init(void *content);
 void	node_add_son(t_node *father, t_node *neww);
 void	node_print_recurse(t_node *root);
-=======
-t_three	*three_init(void *content);
-//void	*three_add(t_three **father, three *son);
+
+//	envp
 char	*set_envp(char *getter, char *new_values, t_core *core);
 char	*get_envp(char *getter, t_core *core);
-
->>>>>>> 4fd51051ba587b8b88fd6a129797d95f6e3638e1
 #endif
