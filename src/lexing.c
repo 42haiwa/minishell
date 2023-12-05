92050407f7272adabfe0e685a6ef2fc3812cc949
Clement Jouenne <cjouenne@c2r12p3.42lehavre.fr>
2023-12-05 19:43:11 +0100

    check empty str

--- src/lexing.c
+++ src/lexing.c
@@ -6,7 +6,7 @@
 /*   By: cjouenne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 18:22:30 by cjouenne          #+#    #+#             */
-/*   Updated: 2023/12/05 19:26:28 by cjouenne         ###   ########.fr       */
+/*   Updated: 2023/12/05 19:38:18 by cjouenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,24 +56,24 @@ void	lexing(char const *buf, t_core *core)
 			buf_w_delimiter = ft_strjoin(buf_w_delimiter, "SEMICOLON");
 			continue ;
 		}
-		if (ft_strncmp(buf_splited[i], ">", 1) == 0)
-		{
-			buf_w_delimiter = ft_strjoin(buf_w_delimiter, "GREAT");
-			continue ;
-		}
 		if (ft_strncmp(buf_splited[i], ">>", 1) == 0)
 		{
 			buf_w_delimiter = ft_strjoin(buf_w_delimiter, "GREATGREAT");
 			continue ;
 		}
-		if (ft_strncmp(buf_splited[i], "<", 1) == 0)
+		if (ft_strncmp(buf_splited[i], ">", 1) == 0)
 		{
-			buf_w_delimiter = ft_strjoin(buf_w_delimiter, "LESS");
+			buf_w_delimiter = ft_strjoin(buf_w_delimiter, "GREAT");
 			continue ;
 		}
 		if (ft_strncmp(buf_splited[i], "<<", 1) == 0)
 		{
 			buf_w_delimiter = ft_strjoin(buf_w_delimiter, "LESSLESS");
+			continue ;
+		}
+		if (ft_strncmp(buf_splited[i], "<", 1) == 0)
+		{
+			buf_w_delimiter = ft_strjoin(buf_w_delimiter, "LESS");
 			continue ;
 		}
 		if (buf_w_delimiter == NULL)
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: aallou-v <aallou-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 16:37:49 by cjouenne          #+#    #+#             */
-/*   Updated: 2023/12/05 19:20:32 by cjouenne         ###   ########.fr       */
+/*   Updated: 2023/12/05 19:35:15 by cjouenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ int	main(int argc, char *argv[], char *envp[])
 	while (1)
 	{
 		buf = readline(get_prompt(core));
+		if (buf[0] == 0)
+			continue ;
 		lexing(buf, core);
 		check_builtins(buf, core);
 	}
