fa4caa0d5d3591fc47de42d0ba0d3fe0b37ed537
Clement Jouenne <cjouenne@c1r6p5.42lehavre.fr>
2023-12-07 17:21:46 +0100

    args execve

--- src/execution.c
+++ src/execution.c
@@ -6,7 +6,7 @@
 /*   By: cjouenne <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 16:03:06 by cjouenne          #+#    #+#             */
-/*   Updated: 2023/12/07 16:49:08 by cjouenne         ###   ########.fr       */
+/*   Updated: 2023/12/07 17:15:48 by cjouenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,18 @@ void	execution(t_core *core)
 	int		pipe_fd[2];
 	pid_t	c_pid;
 	char	buf;
+	ssize_t	i;
 
+	printf("%zu\n", core->execution_three->sons[0]->sons_ctr);
+	char *new_argv[core->execution_three->sons[0]->sons_ctr + 2];
+	i = 1;
+	while (i <= core->execution_three->sons[0]->sons_ctr)
+	{
+		new_argv[i] = (char *) core->execution_three->sons[0]->sons[i - 1]->content;
+		i++;
+	}
+	new_argv[0] = (char *) core->execution_three->sons[0]->content;
+	new_argv[core->execution_three->sons[0]->sons_ctr + 1] = NULL;
 	// 0 IN
 	// 1 OUT
 	pipe(pipe_fd);
@@ -27,7 +38,6 @@ void	execution(t_core *core)
 		close(pipe_fd[0]);
 		dup2(pipe_fd[1], STDOUT_FILENO);
 		close(pipe_fd[1]);
-		char *new_argv[] = {core->execution_three->sons[0]->content, NULL};		
 		execve((char *) core->execution_three->sons[0]->content, new_argv, core->envp);
 		
 		perror("execve");
