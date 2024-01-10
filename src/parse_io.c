c3e44be9810cac6d11408f4b672f94d49b7d0f98
42haiwa <clement222206@gmail.com>
2024-01-10 12:42:45 +0100

    >> work

--- src/parse_io.c
+++ src/parse_io.c
@@ -9,11 +9,15 @@ void	parse_io(t_core *core)
 	while (i < core->execution_three->sons_ctr)
 	{
 		if (ft_strncmp(core->execution_three->sons[i]->content, "PIPE", 4) == 0)
-			break ;
+		{
+			i++;
+			continue ;
+		}
 		if ((i + 2 < core->execution_three->sons_ctr) && ft_strncmp(core->execution_three->sons[i + 1]->content, "GREAT", 5) == 0
 			&& ft_strncmp(core->execution_three->sons[i + 2]->content, "GREAT", 5) == 0)
 		{
 			path = ft_substr(core->execution_three->sons[i + 4]->content, 1, ft_strlen(core->execution_three->sons[i + 4]->content) - 2);
+			printf("path: %s\n", path);
 			core->execution_three->sons[i]->output_fd = open(path, O_WRONLY | O_CREAT | O_APPEND, 0644);
 			core->execution_three->sons[i + 1]->content = "\0";
 			core->execution_three->sons[i + 2]->content = "\0";
