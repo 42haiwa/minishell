8872f59653a2f603b2dc61ea4ab9537786e60c60
Antoine Allou Vincheneux <aallou-v@c1r7p6.42lehavre.fr>
2024-03-27 19:30:45 +0100

    patch

--- src/replace_env.c
+++ src/replace_env.c
@@ -6,7 +6,7 @@
 /*   By: aallou-v <aallou-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 12:15:41 by aallou-v          #+#    #+#             */
-/*   Updated: 2024/03/27 17:51:25 by aallou-v         ###   ########.fr       */
+/*   Updated: 2024/03/27 19:29:11 by aallou-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,7 +95,7 @@ char	**exctract_env(char *s)
 	{
 		if (s[stru.i] == '$' && s[stru.i + 1] == '?')
 			speciale(&stru, result);
-		else if (s[stru.i] == '$' && s[stru.i + 1] != ' ')
+		else if (s[stru.i] == '$' && s[stru.i + 1] != ' ' && s[stru.i + 1] != '\0')
 			extract_env2(&stru, s, result);
 		else
 			stru.i++;
